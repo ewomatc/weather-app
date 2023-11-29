f104ef1db3f9435e540132fdb398f564ada98888
ewomatc <iamgreatdiro@gmail.com>
2023-11-29 20:27:54 +0100

    complete html and basic layout

---
+++ main.js
@@ -0,0 +1,8 @@
+const API_KEY = 'myapikeyhere';
+
+const getCurrentWeatherData = async () => {
+	const city = 'Lagos';
+	const res = `https://apiurlhereblblablah`;
+};
+
+document.addEventListener('DOMContentLoaded', () => {});
