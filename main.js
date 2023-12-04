647a56beedd9cbb2163cf6f236cae4eb72732f24
ewomatc <iamgreatdiro@gmail.com>
2023-12-04 10:19:40 +0100

    layout

--- main.js
+++ main.js
@@ -1,16 +1,46 @@
 const API_KEY = '26a7a36fb0c93415308af69767c50f15';
+const city = 'Lagos';
 
+// Get current weather data block
 const getCurrentWeatherData = async () => {
-	const city = 'Lagos';
 	const response = await fetch(
 		`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
 	);
 	return response.json();
 };
 
+// Get hourly forecast data
+const getHourlyForecastData = async () => {
+	const response = await fetch(
+		`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`
+	);
+
+	const data = await response.json();
+
+	return data.list.map((forecast) => {
+		const {
+			main,
+			dt,
+			dt_txt,
+			weather: [{ description, icon }],
+		} = forecast;
+
+		return {
+			temp: main.temp,
+			temp_max: main.temp_max,
+			temp_min: main.temp_min,
+			dt,
+			dt_txt,
+			description,
+			icon,
+		};
+	});
+};
+
 // formatTemperature changes the temp to 1 decimal place and gives it the degree symbol
 const formatTemperature = (temp) => `${temp?.toFixed(1)}°`;
 
+//load current forecast
 const loadCurrentForecast = ({ name, main, weather: [{ description }] }) => {
 	const currentForecastElement = document.querySelector('#current-forecast');
 	currentForecastElement.querySelector('.city').textContent = name;
@@ -26,8 +56,14 @@ const loadCurrentForecast = ({ name, main, weather: [{ description }] }) => {
 	)} ***** L: ${formatTemperature(main.temp_min)}`;
 };
 
+// load hourly forecast
+const loadHourlyForecast = (hourlyForecast) => {
+	console.log(hourlyForecast);
+};
+
 document.addEventListener('DOMContentLoaded', async () => {
-	// console.log(await getCurrentWeatherData());
 	const currentWeather = await getCurrentWeatherData();
 	loadCurrentForecast(currentWeather);
+
+	const hourlyForecast = await getHourlyForecastData();
 });
