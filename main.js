7cdd1d4cf5ba217133777353bfe3867a308dee35
ewomatc <iamgreatdiro@gmail.com>
2023-12-15 03:58:58 +0100

    gg

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let selectedCity;
 
 const getCitiesUsingGeolocation = async (searchText) => {
 	const response = await fetch(
-		`http://api.openweathermap.org/geo/1.0/direct?q=${searchText}&limit=5&appid=${API_KEY}`
+		`https://api.openweathermap.org/geo/1.0/direct?q=${searchText}&limit=5&appid=${API_KEY}`
 	);
 	return response.json();
 };
